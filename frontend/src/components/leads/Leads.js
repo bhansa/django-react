be52ff1e8d8cc3df5274f597c5df00d2dbecaeea
bhansa <bsaraswat45@gmail.com>
2020-05-25 19:58:49 +0530

    added store, reducer and leads

--- frontend/src/components/leads/Leads.js
+++ frontend/src/components/leads/Leads.js
@@ -1,9 +1,48 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getLeads } from "../../actions/leads";
 
 class Leads extends Component {
+  static proptypes = {
+    leads: PropTypes.array.isRequired,
+  };
+
+  componentDidMount() {
+    this.props.getLeads();
+  }
+
   render() {
-    return <h2>Leads list</h2>;
+    return (
+      <Fragment>
+        <h2>Leads</h2>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Message</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.leads.map((lead) => (
+              <tr key={lead.id}>
+                <td>{lead.id}</td>
+                <td>{lead.name}</td>
+                <td>{lead.email}</td>
+                <td>{lead.message}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Fragment>
+    );
   }
 }
 
-export default Leads;
+const mapStateToProps = (state) => ({
+  leads: state.leads.leads,
+});
+
+export default connect(mapStateToProps, { getLeads })(Leads);
